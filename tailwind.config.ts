5ef42bba99730e7bef4bf1d009c4d2afe8f4fb53
apple <apple@apples-MacBook-Pro-2.local>
2024-06-26 17:23:12 +0530

    add gallery page and Alumni Gallery page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customYellow: 'rgba(234, 195, 32, 0.25)', 
+      },
+
+
     },
   },
   plugins: [],
