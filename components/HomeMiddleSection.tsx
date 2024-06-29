74ec1c3f2bfd27903b6114a4dafaf2f93fdba0b3
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-06-29 14:54:01 +0530

    home page responsive,created student services page

--- components/HomeMiddleSection.tsx
+++ components/HomeMiddleSection.tsx
@@ -14,34 +14,21 @@ const HomeMiddleSection: React.FC = () => {
                 <div className='flex flex-col items-center gap-5'>
                     <h1 className='font-bold text-2xl underline underline-offset-2'>Alumni Engagement</h1>
                     <p className='text-center font-medium text-lg'>Alumni engagement strategies should start well before students graduate. This helps to build more effective relationships and long-term networks</p>
-                    <div className='grid grid-cols-3 max-sm:grid-cols-2 md:gap-12 gap-6'>
-                        <div className='flex flex-col bg-white items-center p-3 gap-2 border-[#979393] border-[1px] rounded-xl shadow-xl min-w-36 lg:w-72 md:w-56 w-52 cursor-pointer transition duration-200 ease-in-out hover:scale-105 hover:shadow-[0_4px_16px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)]'>
-                            <div className='relative lg:w-64 md:w-48 md:h-48 lg:h-64 w-44 h-44 '>
-                                <img src="/jnvimg2.png" alt="" className='bg-[#7D5B04] object-cover w-full h-full rounded-t-xl absolute' />
-                            </div>
-                            <div className='flex flex-col gap-1'>
-                                <h2 className='text-lg font-semibold'>Networking Opportunities</h2>
-                                <p className='font-medium'>Join our alumni network to connect with graduates, attend events, engage in online forums, and participate in special interest groups.</p>
-                            </div>
-                        </div>
-                        <div className='flex flex-col bg-white items-center p-4 gap-2 border-[#979393] border-[1px] rounded-xl shadow-xl min-w-36 lg:w-72 md:w-56 w-52 cursor-pointer transition duration-200 ease-in-out hover:scale-105 hover:shadow-[0_4px_16px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)] '>
-                            <div className='relative lg:w-64 md:w-48 md:h-48 lg:h-64 w-44 h-44 '>
-                                <img src="/jnvimg3.png" alt="" className='bg-[#d396f8] object-cover w-full h-full rounded-t-xl absolute' />
-                            </div>
-                            <div className='flex flex-col gap-1'>
-                                <h2 className='text-lg font-semibold'>Events and Reunions</h2>
-                                <p className='font-medium'>We host annual reunions, regional meetups, webinars, and workshops to reconnect and network throughout the year.</p>
-                            </div>
-                        </div>
-                        <div className='flex flex-col bg-white items-center p-4 gap-2 border-[#979393] border-[1px] rounded-xl shadow-xl min-w-36 lg:w-72 md:w-56 w-52 cursor-pointer transition duration-200 ease-in-out hover:scale-105 hover:shadow-[0_4px_16px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)] '>
-                            <div className='relative lg:w-64 md:w-48 md:h-48 lg:h-64 w-44 h-44 '>
-                                <img src="/jnvimg4.png" alt="" className='bg-[#231609] object-cover w-full h-full rounded-t-xl absolute' />
-                            </div>
-                            <div className='flex flex-col gap-1'>
-                                <h2 className='text-lg font-semibold'>Alumni Stories</h2>
-                                <p className='font-medium'>Discover inspiring alumni success stories and how JNV shaped their journeys.</p>
-                            </div>
-                        </div>
+                    <div className='grid md:grid-cols-3 max-md:grid-cols-2 lg:gap-12 gap-6 max-[480px]:gap-4 px-2 '>
+                        {alumniEngagementCards.map((card, index) => {
+                            return (
+                                <div key={index} className='flex flex-col col-span-1 bg-white items-center py-3 gap-2 border-[#979393] border-[1px] rounded-xl shadow-xl min-w-36 lg:w-64 w-56 max-sm:w-48 cursor-pointer transition duration-200 ease-in-out hover:scale-105 hover:shadow-[0_4px_16px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)]'>
+                                    <div className='relative min-w-36 lg:w-56 lg:h-56 w-40 h-40 sm:w-48 sm:h-48  '>
+                                        <img style={{backgroundColor:`${card.imgBg}`}} src={card.img} alt={`card ${index+1}`} className='object-cover w-full h-full rounded-t-xl absolute' />
+                                    </div>
+                                    <div className='flex flex-col gap-1 px-3'>
+                                        <h2 className='text-lg max-sm:text-base font-semibold'>{card.title}</h2>
+                                        <p className='text-base max-sm:text-sm font-medium'>{card.description}</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+
                     </div>
                 </div>
             </div>
@@ -63,5 +50,33 @@ const HomeMiddleSection: React.FC = () => {
         </div>
     )
 }
+
+interface AlumniEngagement {
+    img: string;
+    imgBg: string;
+    title: string;
+    description: string;
+}
+
+const alumniEngagementCards:AlumniEngagement[] = [
+    {
+        img: "/jnvimg2.png",
+        imgBg:"#7D5B04",
+        title: "Networking Opportunities",
+        description: "Join our alumni network to connect with graduates, attend events, engage in online forums, and participate in special interest groups.",
+    },
+    {
+        img: "/jnvimg3.png",
+        imgBg:"#d396f8",
+        title: "Events and Reunions",
+        description: "We host annual reunions, regional meetups, webinars, and workshops to reconnect and network throughout the year.",
+    },
+    {
+        img: "/jnvimg4.png",
+        imgBg:"#231609",
+        title: "Alumni Stories",
+        description: "Discover inspiring alumni success stories and how JNV shaped their journeys.",
+    },
+]
 
 export default HomeMiddleSection;
